8d8ba65da4cb3ded536422495e42be72308b0a6f
SewoongLee <lsw1028@snu.ac.kr>
2024-05-24 10:14:55 +0900

    Randomize n-shot samples

--- src/util/gsm8k_helper.py
+++ src/util/gsm8k_helper.py
@@ -1,16 +1,18 @@
+import random
+
+def nshot_prompt(data: list, n: int, question: str) -> str:
+    prompt = ''
+    for qna in random.sample(data, n):
+        prompt += f'''
+        <s>[INST]{qna["question"]}[/INST]
+        Assistant: {qna["answer"]}</s>
+        '''
+
+    return prompt + '<s>[INST]' + question + '[/INST]'  # no end tag </s> here!
+
 def parse_into_num_ans(full_answer: str) -> int:
     delimiter = '####'
     if delimiter not in full_answer:
         return None
     
     return int(full_answer.split(delimiter)[1].replace(',',''))
-
-def nshot_prompt(data: list, n: int, question: str) -> str:
-    prompt = ''
-    for i in range(n):
-        prompt += f'''
-        <s>[INST]{data[i]["question"]}[/INST]
-        Assistant: {data[i]["answer"]}</s>
-        '''
-
-    return prompt + '<s>[INST]' + question + '[/INST]'  # no end tag </s> here!
