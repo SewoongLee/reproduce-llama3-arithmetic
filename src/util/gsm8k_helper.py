c00bf5b35bacc7a9fe805feac6abafd64dc06ad6
SewoongLee <lsw1028@snu.ac.kr>
2024-05-27 00:13:01 +0900

    Converted to pipeline for chat

--- src/util/gsm8k_helper.py
+++ src/util/gsm8k_helper.py
@@ -1,18 +1,37 @@
 import random
 
-def nshot_prompt(data: list, n: int, question: str) -> str:
-    prompt = ''
-    for qna in random.sample(data, n):
-        prompt += f'''
-        <s>[INST]{qna["question"]}[/INST]
-        Assistant: {qna["answer"]}</s>
-        '''
+# def nshot_prompt(nshot_data: list, n: int, question: str, bos='<|begin_of_text|>', eos='<|eot_id|>') -> str:
+#     random.seed(42)
 
-    return prompt + '<s>[INST]' + question + '[/INST]'  # no end tag </s> here!
+#     prompt = ''
+#     for qna in random.sample(nshot_data, n):
+#         prompt += f'''
+#         {bos}[INST]{qna["question"]}[/INST]
+#         Assistant: {qna["answer"]}{eos}
+#         '''
 
-def parse_into_num_ans(full_answer: str) -> int:
-    delimiter = '####'
-    if delimiter not in full_answer:
-        return None
-    
-    return int(full_answer.split(delimiter)[1].replace(',',''))
+#     return prompt + f'''
+#     {bos}[INST]{question}'[/INST]
+#     Assistant: '''  # no eos here!
+
+
+def nshot_chats(nshot_data: list, n: int, question: str) -> dict:
+    random.seed(42)
+
+    chats = []
+    for qna in random.sample(nshot_data, n):
+        chats.append({"role": "user", "content": qna["question"]})
+        chats.append({"role": "assistant", "content": qna["answer"]})
+
+    chats.append({"role": "user", "content": question})
+    return chats
+
+
+def extract_ans_from_response(answer: str, eos=None):
+    if eos:
+        answer = answer.split(eos)[0].strip()
+
+    answer = answer.split('####')[-1].strip()
+    answer = answer.replace(',', '').replace('$', '')
+
+    return answer
